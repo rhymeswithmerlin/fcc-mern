2d39a5a754b7718d4804e513bf34a49f536e1496
Christopher Sherlin <christophersherlin@Christophers-MacBook-Pro.local>
2021-06-26 09:09:15 -0400

    basic create-react-app with simple nav

---
+++ frontend/src/components/add-review.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddReview() {
+  return (
+    <div className="">
+      add review
+    </div>
+  );
+}
+
+export default AddReview;
---
+++ frontend/src/components/restaurants-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RestaurantsList() {
+  return (
+    <div className="">
+      restaurant list
+    </div>
+  );
+}
+
+export default RestaurantsList;
